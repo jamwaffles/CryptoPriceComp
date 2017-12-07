14d9bd7795f2208f255dc0f92b4bde52a0c517e0
James Waples <jamwaffles@gmail.com>
2017-12-07 23:55:34 +0000

    Test route to get XMR -> GBP price
    Talks to Poloniex and GDAX

--- server/routes.mjs
+++ server/routes.mjs
@@ -8,4 +8,22 @@ mainRouter.get('/health', async ctx => {
 	ctx.body = "Nothing to see here"
 })
 
+mainRouter.get('/xmr/gbp', async ctx => {
+	const pres = await fetch('https://poloniex.com/public?command=returnTicker')
+	const { BTC_XMR } = await pres.json()
+
+	const xmrToBtc = parseFloat(BTC_XMR.last)
+
+	const gdaxRes = await fetch('https://api.gdax.com/products/BTC-GBP/ticker')
+	const gdax = await gdaxRes.json()
+
+	const btcToGbp = parseFloat(gdax.price)
+
+	ctx.body = {
+		xmrToBtc,
+		btcToGbp,
+		xmrToGbp: btcToGbp * xmrToBtc,
+	}
+})
+
 export default mainRouter

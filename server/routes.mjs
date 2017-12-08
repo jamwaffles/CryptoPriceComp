57e0dc4ca2fc3ca97f360558c808b36ca1c67589
James Waples <james@cambridge-intelligence.com>
2017-12-08 10:50:12 +0000

    Make requests to Poloniex trading API
    Fuck that's a weird interface

--- server/routes.mjs
+++ server/routes.mjs
@@ -1,6 +1,13 @@
+// @flow
+
 import Router from 'koa-router'
+import crypto from 'crypto'
+import qs from 'querystring'
+import nonceLib from 'nonce'
+const n = nonceLib()
 
 import logger from './logger'
+import config from './config'
 
 const mainRouter = new Router({
 	prefix: '',
@@ -9,6 +16,37 @@ const mainRouter = new Router({
 mainRouter.get('/health', async ctx => {
 	ctx.body = "Nothing to see here"
 })
+
+async function getPoloniexTradingFees() {
+	const nonce = n()
+
+	const params = qs.stringify({
+		nonce,
+		command: 'returnFeeInfo',
+	})
+
+	let hash = crypto.createHmac('sha512', config('POLONIEX_API_SECRET'))
+	hash.update(params)
+	const sign = hash.digest('hex')
+	console.log(sign)
+
+	const headers = new Headers({
+		'Content-Type': 'application/x-www-form-urlencoded',
+		Key: config('POLONIEX_API_KEY'),
+		Sign: sign,
+		'User-Agent': 'Crypto Comp',
+	})
+
+	const fullUrl = `https://poloniex.com/tradingApi?${params}`
+
+	logger.debug('poloniexApiRequest', { fullUrl, method: 'POST', headers, body: params })
+
+	const res = await fetch(fullUrl, { method: 'POST', headers, body: params })
+
+	logger.debug('poloniexApiResponse', { status: res.status })
+
+	return await res.json()
+}
 
 async function getGdaxPairs() {
 	const res = await fetch('https://api.gdax.com/products')
@@ -43,9 +81,10 @@ mainRouter.get('/xmr/gbp', async ctx => {
 
 	// const btcToGbp = parseFloat(gdax.price)
 
-	const pairs = await getGdaxPairs()
+	// const pairs = await getGdaxPairs()
 
-	ctx.body = pairs
+	// ctx.body = pairs
+	ctx.body = await getPoloniexTradingFees()
 
 	// ctx.body = {
 	// 	xmrToBtc,

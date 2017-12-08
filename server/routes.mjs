945779ba2b3edae0e5d35651f55d5fb8f3ab3686
James Waples <jamwaffles@gmail.com>
2017-12-08 00:35:17 +0000

    PoC XMR -> BTC -> GBP single path
    Gonna start doing graphy shit tomorrow

--- server/routes.mjs
+++ server/routes.mjs
@@ -1,5 +1,7 @@
 import Router from 'koa-router'
 
+import logger from './logger'
+
 const mainRouter = new Router({
 	prefix: '',
 })
@@ -8,22 +10,48 @@ mainRouter.get('/health', async ctx => {
 	ctx.body = "Nothing to see here"
 })
 
-mainRouter.get('/xmr/gbp', async ctx => {
-	const pres = await fetch('https://poloniex.com/public?command=returnTicker')
-	const { BTC_XMR } = await pres.json()
+async function getGdaxPairs() {
+	const res = await fetch('https://api.gdax.com/products')
+	const products = await res.json()
 
-	const xmrToBtc = parseFloat(BTC_XMR.last)
+	logger.info('gdaxProductsList', products)
 
-	const gdaxRes = await fetch('https://api.gdax.com/products/BTC-GBP/ticker')
-	const gdax = await gdaxRes.json()
+	let pairs = []
 
-	const btcToGbp = parseFloat(gdax.price)
+	for(let product of products) {
+		const prod = await fetch(`https://api.gdax.com/products/${product.id}/ticker`)
+		const { price } = await prod.json()
 
-	ctx.body = {
-		xmrToBtc,
-		btcToGbp,
-		xmrToGbp: btcToGbp * xmrToBtc,
+		pairs.push({
+			from: product.base_currency,
+			to: product.quote_currency,
+			price,
+		})
 	}
+
+	return pairs
+}
+
+mainRouter.get('/xmr/gbp', async ctx => {
+	// const pres = await fetch('https://poloniex.com/public?command=returnTicker')
+	// const { BTC_XMR } = await pres.json()
+
+	// const xmrToBtc = parseFloat(BTC_XMR.last)
+
+	// const gdaxRes = await fetch('https://api.gdax.com/products/BTC-GBP/ticker')
+	// const gdax = await gdaxRes.json()
+
+	// const btcToGbp = parseFloat(gdax.price)
+
+	const pairs = await getGdaxPairs()
+
+	ctx.body = pairs
+
+	// ctx.body = {
+	// 	xmrToBtc,
+	// 	btcToGbp,
+	// 	xmrToGbp: btcToGbp * xmrToBtc,
+	// }
 })
 
 export default mainRouter

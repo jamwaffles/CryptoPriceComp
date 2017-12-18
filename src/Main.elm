3880430ddfb3c010e806c293c6bd3e8b242de5d4
James Waples <jamwaffles@gmail.com>
2017-12-18 00:04:38 +0000

    WIP Elm types

--- src/Main.elm
+++ src/Main.elm
@@ -3,5 +3,23 @@ module Main exposing (..)
 import Html exposing (text)
 
 
+type alias Exchange =
+    { currency : String
+    , source : String
+    , last : Maybe Int
+    }
+
+
+type alias ExchangePath =
+    List Exchange
+
+
+type alias Model =
+    { startCurrency : String
+    , endCurrency : String
+    , path : Maybe ExchangePath
+    }
+
+
 main =
     text "I'm an Elm app!"
